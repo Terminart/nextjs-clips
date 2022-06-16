eb6d904b0d774e4c2fca1ca11eb9e4d4de616791
Terminart <74411018+Terminart@users.noreply.github.com>
2022-06-16 12:37:36 +0900

    chore: add motion top page

---
+++ next/src/pages/motion/index.tsx
@@ -0,0 +1,32 @@
+import type { NextPage } from 'next'
+import { CategoryTop } from '@/components/templates/CategoryTop'
+import { SquareLinkProps } from '@/components/atoms/SquareLink'
+
+const sampleLinks: SquareLinkProps[] = [
+  {
+    href: '',
+    image: '/gallery.jpg',
+    title: 'test1',
+  },
+  {
+    href: '',
+    image: '/gallery.jpg',
+    title: 'test3',
+  },
+  {
+    href: '',
+    image: '/integration.jpg',
+    title: 'test4',
+  },
+  {
+    href: '',
+    image: '/motion.jpg',
+    title: 'test5',
+  },
+]
+
+const Page: NextPage = () => {
+  return <CategoryTop links={sampleLinks} />
+}
+
+export default Page
