35d4b602253f5c5050388490f14b6ffce8fb8981
Terminart <74411018+Terminart@users.noreply.github.com>
2022-06-16 12:37:53 +0900

    chore: add integration top page

---
+++ next/src/pages/integration/index.tsx
@@ -0,0 +1,27 @@
+import type { NextPage } from 'next'
+import { CategoryTop } from '@/components/templates/CategoryTop'
+import { SquareLinkProps } from '@/components/atoms/SquareLink'
+
+const sampleLinks: SquareLinkProps[] = [
+  {
+    href: '',
+    image: '/gallery.jpg',
+    title: 'test1',
+  },
+  {
+    href: '',
+    image: '/motion.jpg',
+    title: 'test2',
+  },
+  {
+    href: '',
+    image: '/gallery.jpg',
+    title: 'test3',
+  },
+]
+
+const Page: NextPage = () => {
+  return <CategoryTop links={sampleLinks} />
+}
+
+export default Page
