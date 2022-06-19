340c8f197d144e2edfb9f3291f3fc13174a0145d
Terminart <74411018+Terminart@users.noreply.github.com>
2022-06-19 09:05:45 +0900

    refactor: links definitions

--- next/src/lib/path.ts
+++ next/src/lib/path.ts
@@ -1,45 +1,52 @@
 import { LinkWithImageProps } from '@/types/link'
 import { NextRouter } from 'next/router'
+import _ from 'lodash'
 
-export const homeLink: LinkWithImageProps = {
-  href: '/',
-  title: 'Top',
-  image: '',
+type Categories =
+  | 'home'
+  | 'gallery'
+  | 'motion'
+  | 'integration'
+  | 'login'
+  | 'signup'
+export const links: Record<Categories, LinkWithImageProps> = {
+  home: {
+    href: '/',
+    title: 'Top',
+    image: '',
+  },
+  gallery: {
+    href: '/gallery',
+    title: 'Gallery',
+    image: '/gallery.jpg',
+  },
+  motion: {
+    href: '/motion',
+    title: 'Motion',
+    image: '/motion.jpg',
+  },
+  integration: {
+    href: '/integration',
+    title: 'Integration',
+    image: '/integration.jpg',
+  },
+  login: {
+    href: '/login',
+    title: 'Login',
+    image: '',
+  },
+  signup: {
+    href: '/signup',
+    title: 'SignUp',
+    image: '',
+  },
 }
-export const galleryLink: LinkWithImageProps = {
-  href: '/gallery',
-  title: 'Gallery',
-  image: '/gallery.jpg',
-}
-export const motionLink: LinkWithImageProps = {
-  href: '/motion',
-  title: 'Motion',
-  image: '/motion.jpg',
-}
-export const integrationLink: LinkWithImageProps = {
-  href: '/integration',
-  title: 'Integration',
-  image: '/integration.jpg',
-}
-export const loginLink: LinkWithImageProps = {
-  href: '/login',
-  title: 'Login',
-  image: '',
-}
-export const signupLink: LinkWithImageProps = {
-  href: '/signup',
-  title: 'SignUp',
-  image: '',
-}
-export const headerLinks = [homeLink, galleryLink, motionLink, integrationLink]
-export const menuLinks = [
-  homeLink,
-  galleryLink,
-  motionLink,
-  integrationLink,
-  loginLink,
-  signupLink,
-]
+
+export const headerLinks = _.chain(links)
+  .pick(['home', 'gallery', 'motion', 'integration'])
+  .values()
+  .value()
+export const menuLinks = _.chain(links).values().value()
 
 export function isHrefInCategory(href: string, router: NextRouter) {
   return href === router.pathname.split(/(?=\/)/g)[0]
