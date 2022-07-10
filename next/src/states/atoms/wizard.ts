4a7e487dee5bc14c0cad97005c3373b51d3f42e8
Terminart <74411018+Terminart@users.noreply.github.com>
2022-07-10 12:34:18 +0900

    refactor(Wizard): reset atom when route is changed

--- next/src/states/atoms/wizard.ts
+++ next/src/states/atoms/wizard.ts
@@ -1,4 +1,6 @@
 import { atom } from 'recoil'
+import Router from 'next/router'
+import _ from 'lodash'
 
 export type WizardInputs = {
   name: string
@@ -13,4 +15,15 @@ export const wizardInputsAtom = atom<WizardInputs>({
     age: 0,
     country: undefined,
   },
+  effects: [
+    ({ resetSelf }) => {
+      // TODO: atomを破棄できる機能が追加されたらリファクタリングする
+      Router.events.on('routeChangeComplete', (path) => {
+        if (!_.startsWith(path, '/integration/wizard')) {
+          console.log('reset')
+          resetSelf()
+        }
+      })
+    },
+  ],
 })
