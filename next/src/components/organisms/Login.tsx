b1343772e03e499048c71ea298ed2ead61187dc6
Terminart <74411018+Terminart@users.noreply.github.com>
2022-07-10 15:27:23 +0900

    feat(Login,Signup): use formik

--- next/src/components/organisms/Login.tsx
+++ next/src/components/organisms/Login.tsx
@@ -1,17 +1,34 @@
 import { Button, Center, Heading, VStack } from '@chakra-ui/react'
-import { FormInput } from '@/components/molecules/FormInput'
+import { FormInputTemp } from '@/components/molecules/FormInputTemp'
+import { Form, Formik } from 'formik'
+import * as React from 'react'
 
 export const Login = () => {
   return (
     <>
       <Heading>Login</Heading>
-      <VStack align={'baseline'} spacing={8} px={4} py={{ base: 6, md: 8 }}>
-        <FormInput label={'Name'} />
-        <FormInput label={'Password'} type={'password'} />
-      </VStack>
-      <Center mt={8}>
-        <Button mx={'auto'}>Submit</Button>
-      </Center>
+      <Formik
+        initialValues={{}}
+        onSubmit={() => {
+          alert('not implemented')
+        }}
+      >
+        <Form>
+          <VStack align={'baseline'} spacing={8} px={4} py={{ base: 6, md: 8 }}>
+            <FormInputTemp id={'name'} label={'Name'} />
+            <FormInputTemp
+              id={'password'}
+              label={'Password'}
+              type={'password'}
+            />
+          </VStack>
+          <Center mt={8}>
+            <Button type={'submit'} mx={'auto'}>
+              Submit
+            </Button>
+          </Center>
+        </Form>
+      </Formik>
     </>
   )
 }
--- next/src/components/organisms/SignUp.tsx
+++ next/src/components/organisms/SignUp.tsx
@@ -1,18 +1,38 @@
 import { Button, Center, Heading, VStack } from '@chakra-ui/react'
-import { FormInput } from '@/components/molecules/FormInput'
+import { Form, Formik } from 'formik'
+import { FormInputTemp } from '@/components/molecules/FormInputTemp'
 
 export const SignUp = () => {
   return (
     <>
       <Heading>SignUp</Heading>
-      <VStack align={'baseline'} spacing={8} px={4} py={{ base: 6, md: 8 }}>
-        <FormInput label={'Name'} />
-        <FormInput label={'Password'} type={'password'} />
-        <FormInput label={'Confirm Password'} type={'password'} />
-      </VStack>
-      <Center mt={8}>
-        <Button mx={'auto'}>SignUp</Button>
-      </Center>
+      <Formik
+        initialValues={{}}
+        onSubmit={() => {
+          alert('not implemented')
+        }}
+      >
+        <Form>
+          <VStack align={'baseline'} spacing={8} px={4} py={{ base: 6, md: 8 }}>
+            <FormInputTemp id={'name'} label={'Name'} />
+            <FormInputTemp
+              id={'password'}
+              label={'Password'}
+              type={'password'}
+            />
+            <FormInputTemp
+              id={'confirmPassword'}
+              label={'Confirm Password'}
+              type={'password'}
+            />
+          </VStack>
+          <Center mt={8}>
+            <Button type={'submit'} mx={'auto'}>
+              SignUp
+            </Button>
+          </Center>
+        </Form>
+      </Formik>
     </>
   )
 }
