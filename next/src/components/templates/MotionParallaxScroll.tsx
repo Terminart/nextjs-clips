6b4d8e6c696906acf2100b56ace124e7d686b83e
Terminart <74411018+Terminart@users.noreply.github.com>
2022-07-30 13:35:49 +0900

    refactor(Motion): refactor ParallaxScroll page

--- next/src/components/templates/MotionParallaxScroll.tsx
+++ next/src/components/templates/MotionParallaxScroll.tsx
@@ -1,48 +1,41 @@
-import {
-  Box,
-  forwardRef,
-  Heading,
-  useColorModeValue,
-  VStack,
-} from '@chakra-ui/react'
+import { Box, Heading, useColorModeValue, VStack } from '@chakra-ui/react'
 import { motion, useTransform, useViewportScroll } from 'framer-motion'
 import Image from 'next/image'
 import { DummyText } from '@/components/molecules/DummyText'
 
 export const MotionParallaxScroll = () => {
+  const color = useColorModeValue('gray.200', 'gray.600')
+
   const { scrollY } = useViewportScroll()
   const parallaxY = useTransform(scrollY, [0, 100], [0, -80], { clamp: false })
 
   return (
     <Box pos={'relative'}>
-      <ParallaxFront />
-      <ParallaxBackground style={{ y: parallaxY }} />
-    </Box>
-  )
-}
-
-const ParallaxFront = () => {
-  const color = useColorModeValue('gray.200', 'gray.600')
-
-  return (
-    <VStack w={'full'} pos={'relative'} zIndex={10}>
-      <Box w={'full'} h={{ base: '400px', sm: '600px' }} bgColor={color} p={10}>
-        <Heading>Lorem ipsum dolor sit amet</Heading>
-        <DummyText paragraphs={3} noOfLines={{ base: 9, sm: 17 }} />
-      </Box>
-      <Box w={'full'} h={{ base: '400px', sm: '700px' }} bg={'transparent'} />
-      <Box w={'full'} h={{ base: '400px', sm: '600px' }} bg={color} p={10}>
-        <Heading>Lorem ipsum dolor sit amet</Heading>
-        <DummyText paragraphs={3} noOfLines={{ base: 9, sm: 17 }} />
-      </Box>
-    </VStack>
-  )
-}
-
-const ParallaxBackground = motion(
-  forwardRef((props, ref) => {
-    return (
-      <Box w={'full'} pos={'absolute'} top={400} zIndex={5} ref={ref}>
+      <VStack w={'full'} pos={'relative'} zIndex={10}>
+        <Box
+          w={'full'}
+          h={{ base: '400px', sm: '600px' }}
+          bgColor={color}
+          p={10}
+        >
+          <Heading>Lorem ipsum dolor sit amet</Heading>
+          <DummyText paragraphs={3} noOfLines={{ base: 9, sm: 17 }} />
+        </Box>
+        <Box w={'full'} h={{ base: '400px', sm: '700px' }} bg={'transparent'} />
+        <Box w={'full'} h={{ base: '400px', sm: '600px' }} bg={color} p={10}>
+          <Heading>Lorem ipsum dolor sit amet</Heading>
+          <DummyText paragraphs={3} noOfLines={{ base: 9, sm: 17 }} />
+        </Box>
+      </VStack>
+      <motion.div
+        style={{
+          width: '100%',
+          position: 'absolute',
+          top: 400,
+          zIndex: 5,
+          y: parallaxY,
+        }}
+      >
         <Box w={'full'} h={{ base: '1100px', sm: '2000px' }} pos={'relative'}>
           <Image
             src={'/universe-splash.jpg'}
@@ -59,7 +52,7 @@ const ParallaxBackground = motion(
             alt={'background image'}
           />
         </Box>
-      </Box>
-    )
-  })
-)
+      </motion.div>
+    </Box>
+  )
+}
