80761d675e0f0d1897a02527d0bb5e8219a9d7b5
Terminart <74411018+Terminart@users.noreply.github.com>
2022-08-09 13:00:46 +0900

    feat(functions): modify news api and test

--- functions/__tests__/api/news.ts
+++ functions/__tests__/api/news.ts
@@ -1,14 +1,29 @@
 import 'dotenv/config'
-import test = require('firebase-functions-test')
 import { api } from '../../src/index'
+import test = require('firebase-functions-test')
 
 const wrap = test({
   projectId: process.env.FIREBASE_PROJECT_ID,
 }).wrap
 
-describe('test', () => {
-  it('should be success', async function () {
-    const result = await wrap(api.news.create)(null)
-    expect(result).toBe('news create')
+describe('api.news', () => {
+  it('should be created without error', async function () {
+    await expect(
+      wrap(api.news.create)({
+        title: 'title',
+        text: 'text',
+        tag: ['tag a', 'tag b'],
+      })
+    ).resolves.not.toThrowError()
+  })
+
+  it('should be thrown error', async function () {
+    await expect(
+      wrap(api.news.create)({
+        title: undefined,
+        text: 'text',
+        tag: ['tag a', 'tag b'],
+      })
+    ).rejects.toThrowError()
   })
 })
--- functions/src/api/news.ts
+++ functions/src/api/news.ts
@@ -1,7 +1,36 @@
 import * as admin from 'firebase-admin'
 import * as functions from 'firebase-functions'
+import { firestore } from 'firebase-admin'
+import Timestamp = firestore.Timestamp
 
-export const create = functions.https.onCall(async () => {
-  await admin.firestore().collection('news').doc('test').set({ a: 1, b: 2 })
-  return 'news create'
+// TODO: use common types
+type News = {
+  id: string
+  title: string
+  text: string
+  tag: string[]
+  createdAt: Timestamp
+  updatedAt: Timestamp
+}
+type NewsRequest = Pick<News, 'title' | 'tag' | 'text'>
+
+export const create = functions.https.onCall(async (request: NewsRequest) => {
+  await admin
+    .firestore()
+    .collection('news') // TODO: use common constants
+    .doc()
+    .create({
+      title: request.title,
+      text: request.text,
+      tag: request.tag,
+      createdAt: Timestamp.now(),
+      updatedAt: Timestamp.now(),
+    })
+    .catch((error) => {
+      throw new functions.https.HttpsError(
+        'internal',
+        'failed to create document "news"',
+        error
+      )
+    })
 })
