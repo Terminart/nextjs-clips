893c6de3ebc922c09040b6652f1ae5269f3f66ed
Terminart <74411018+Terminart@users.noreply.github.com>
2022-08-13 09:15:57 +0900

    feat(next): add yup validation in signup and login pages

--- next/src/components/organisms/Login.tsx
+++ next/src/components/organisms/Login.tsx
@@ -4,6 +4,8 @@ import { Form, Formik } from 'formik'
 import * as React from 'react'
 import { getAuth, signInWithEmailAndPassword } from '@firebase/auth'
 import Router from 'next/router'
+import { object } from 'yup'
+import { emailSchema, passwordSchema } from '@/lib/validation'
 
 export const Login = () => {
   return (
@@ -11,8 +13,13 @@ export const Login = () => {
       <Heading>Login</Heading>
       <Formik
         initialValues={{ email: '', password: '' }}
-        onSubmit={(values) => {
+        validationSchema={object({
+          email: emailSchema,
+          password: passwordSchema,
+        })}
+        onSubmit={(values, formikHelpers) => {
           alert('this function is NOT available now.')
+          formikHelpers.setSubmitting(false)
           return
 
           signInWithEmailAndPassword(getAuth(), values.email, values.password)
@@ -25,17 +32,40 @@ export const Login = () => {
             })
         }}
       >
-        <Form>
-          <VStack align={'baseline'} spacing={8} px={4} py={{ base: 6, md: 8 }}>
-            <FormInput id={'email'} label={'Email'} />
-            <FormInput id={'password'} label={'Password'} type={'password'} />
-          </VStack>
-          <Center mt={8}>
-            <Button type={'submit'} mx={'auto'}>
-              Submit
-            </Button>
-          </Center>
-        </Form>
+        {({ errors, touched, isSubmitting, isValid }) => (
+          <Form>
+            <VStack
+              align={'baseline'}
+              spacing={8}
+              px={4}
+              py={{ base: 6, md: 8 }}
+            >
+              <FormInput
+                id={'email'}
+                label={'Email'}
+                error={errors.email}
+                touched={touched.email}
+              />
+              <FormInput
+                id={'password'}
+                label={'Password'}
+                type={'password'}
+                error={errors.password}
+                touched={touched.password}
+              />
+            </VStack>
+            <Center mt={8}>
+              <Button
+                type={'submit'}
+                mx={'auto'}
+                isLoading={isSubmitting}
+                disabled={!isValid}
+              >
+                Submit
+              </Button>
+            </Center>
+          </Form>
+        )}
       </Formik>
     </>
   )
--- next/src/components/organisms/SignUp.tsx
+++ next/src/components/organisms/SignUp.tsx
@@ -3,15 +3,27 @@ import { Form, Formik } from 'formik'
 import { FormInput } from '@/components/molecules/FormInput'
 import { createUserWithEmailAndPassword, getAuth } from '@firebase/auth'
 import Router from 'next/router'
+import { object } from 'yup'
+import {
+  confirmPasswordSchema,
+  emailSchema,
+  passwordSchema,
+} from '@/lib/validation'
 
 export const SignUp = () => {
   return (
     <>
       <Heading>SignUp</Heading>
       <Formik
-        initialValues={{ email: '', password: '' }}
-        onSubmit={(values) => {
+        initialValues={{ email: '', password: '', confirmPassword: '' }}
+        validationSchema={object({
+          email: emailSchema,
+          password: passwordSchema,
+          confirmPassword: confirmPasswordSchema,
+        })}
+        onSubmit={(values, formikHelpers) => {
           alert('this function is NOT available now.')
+          formikHelpers.setSubmitting(false)
           return
 
           createUserWithEmailAndPassword(
@@ -29,22 +41,47 @@ export const SignUp = () => {
           alert('ユーザを作成しました。')
         }}
       >
-        <Form>
-          <VStack align={'baseline'} spacing={8} px={4} py={{ base: 6, md: 8 }}>
-            <FormInput id={'email'} label={'Email'} />
-            <FormInput id={'password'} label={'Password'} type={'password'} />
-            <FormInput
-              id={'confirmPassword'}
-              label={'Confirm Password'}
-              type={'password'}
-            />
-          </VStack>
-          <Center mt={8}>
-            <Button type={'submit'} mx={'auto'}>
-              SignUp
-            </Button>
-          </Center>
-        </Form>
+        {({ errors, touched, isSubmitting, isValid }) => (
+          <Form>
+            <VStack
+              align={'baseline'}
+              spacing={8}
+              px={4}
+              py={{ base: 6, md: 8 }}
+            >
+              <FormInput
+                id={'email'}
+                label={'Email'}
+                error={errors.email}
+                touched={touched.email}
+              />
+              <FormInput
+                id={'password'}
+                label={'Password'}
+                type={'password'}
+                error={errors.password}
+                touched={touched.password}
+              />
+              <FormInput
+                id={'confirmPassword'}
+                label={'Confirm Password'}
+                type={'password'}
+                error={errors.confirmPassword}
+                touched={touched.confirmPassword}
+              />
+            </VStack>
+            <Center mt={8}>
+              <Button
+                type={'submit'}
+                mx={'auto'}
+                isLoading={isSubmitting}
+                disabled={!isValid}
+              >
+                SignUp
+              </Button>
+            </Center>
+          </Form>
+        )}
       </Formik>
     </>
   )
